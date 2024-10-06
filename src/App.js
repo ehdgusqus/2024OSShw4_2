2d2bc28ea2b5d6cb29c5252fc93d31953a8b49ba
ehdgusqus <ehdgusqus55@gmail.com>
2024-10-06 20:19:02 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Create from './components/Create';
 import Read from './components/Read';
-import Update from './components/Updated';
+import Update from './components/Update';
 import './App.css';
 
 function App() {
   return (
-      <Router>
-        <div className="main">
-          <div className="content">
-            <h2 className="main-header">React Crud Operations</h2>
-            <div>
-              <Routes>
-                <Route exact path='/' Component={Create} />
-              </Routes>
-              <div style={{ marginTop: 20 }}>
-                <Routes>
-                  <Route exact path='/read' Component={Read} />
-                </Routes>
-              </div>
-              <Routes>
-                <Route path='/update' Component={Update} />
-              </Routes>
-            </div>
-          </div>
+    <Router>
+      <div className="main">
+        <div className="content">
+          <h2 className="main-header">React Crud Operations</h2>
+          <Routes>
+            <Route exact path='/' Component={Create} />
+            <Route exact path='/read' Component={Read} />
+            <Route path='/update' Component={Update} />
+          </Routes>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 }
 
